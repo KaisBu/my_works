f562199a4dfd556b2d2dd8752f664a65ac0ddcb6
Kais <kais.bu-hasan@yandex.ru>
2021-05-05 12:27:39 +0300

    added tasks 2,3,4

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+def sum_digit(n):
+    summ = 0
+    for symbol in str(n):
+        summ += int(symbol)
+    return summ
+
+
+def numb_digit(n):
+    count_d = 0
+    for symbol in str(n):
+        count_d += 1
+    return count_d
+
+
+N = int(input('Enter the number: '))
+
+summ = sum_digit(N)
+count = numb_digit(N)
+
+print('\nSum digits:', summ)
+print('Number of digits:', count)
+print('Difference between the sum and the number of digits:', summ - count)
+
--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -1,3 +1,40 @@
-# TODO здесь писать код
+def invert_symbol(numb):
+    digits = ''
+    for symbol in reversed(numb):
+        if symbol == '.':
+            break
+        digits += symbol
+    return digits
+
+
+def whole_part(numb):
+    digits = ''
+    for symbol in numb:
+        if symbol == '.':
+            break
+        digits += symbol
+    return float(invert_symbol(digits))
+
+
+def after_point(numb):
+    count = 0
+    digit = invert_symbol(numb)
+    for symbol in digit:
+        count += 1
+    return float(digit) / 10 ** count
+
+
+numb1 = float(input('\nEnter first number: '))
+numb2 = float(input('Enter second number: '))
+
+numb1 = whole_part(str(numb1)) + after_point(str(numb1))
+numb2 = whole_part(str(numb2)) + after_point(str(numb2))
+
+print('\nFirst reversed number:', numb1)
+print('Second reversed number:', numb2)
+print('Sum:', numb1 + numb2)
+
+
+
 
 
