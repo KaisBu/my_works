9993acda2b40db20afa068357ab0e85525e8f75e
Kais <kais.bu-hasan@yandex.ru>
2021-05-21 15:31:40 +0300

    added tasks 2-6

--- Module16/02_line/main.py
+++ Module16/02_line/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+class_1 = []
+class_2 = []
+
+for num in range(160, 177, 2):
+    class_1.append(num)
+
+for num in range(162, 181, 3):
+    class_2.append(num)
+
+class_1.extend(class_2)
+class_1.sort()
+print('Sorted list:', class_1)
