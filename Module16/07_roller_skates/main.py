0b9bf15304746e90a1df84eec79319bbbc1a58b6
Kais <kais.bu-hasan@yandex.ru>
2021-05-24 15:31:38 +0300

    added tasks 7-10

--- Module16/07_roller_skates/main.py
+++ Module16/07_roller_skates/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+def list_create(num, size):
+    lst = []
+    for i in range(num):
+        element = int(input(f'Enter {size} number {i + 1}: '))
+        lst.append(element)
+    return lst
+
+
+number_rollers = int(input('Enter total number of rollers skates: '))
+roller_size_list = list_create(number_rollers, 'size of the pare')
+
+number_people = int(input('\nEnter number of peoples: '))
+people_size_list = list_create(number_people, 'human foot size')
+count = 0
+
+for foot_size in people_size_list:
+    for roller_size in roller_size_list:
+        if roller_size >= foot_size:
+            count += 1
+            roller_size_list.remove(roller_size)
+            break
+
+print('\nThe largest number of people who can take roller skates', count)
--- Module16/09_friends/main.py
+++ Module16/09_friends/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+number_friend = int(input('Enter total number of friends: '))
+number_iou = int(input('Enter number of IOUs: '))
+iou_list = []
+
+for _ in range(number_friend):
+    iou_list.append(0)
+
+for i in range(number_iou):
+    print('\nIOU  number', i + 1)
+    index1 = int(input('To whom: '))
+    index2 = int(input('By whom: '))
+    summ = int(input('How much: '))
+    iou_list[index1 - 1] -= summ
+    iou_list[index2 - 1] += summ
+
+print('\nFriends balance:')
+for num in iou_list:
+    print(iou_list.index(num) + 1, ':', num)
+
+
+
+
