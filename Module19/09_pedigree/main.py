7ec1e6fb67f4145deccb2fe9b3443ceaf67fd66b
Kais <kais.bu-hasan@yandex.ru>
2021-06-14 04:22:30 +0300

    tasks 9..10 added

--- Module19/09_pedigree/main.py
+++ Module19/09_pedigree/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+def names_height(dictionary, people_name):
+    if people_name not in dictionary.keys():
+        return 0
+    else:
+        return 1 + names_height(dictionary, dictionary[people_name])
+
+
+num_people = int(input('Enter number of people: '))
+tree = dict()
+height = dict()
+
+for i in range(num_people - 1):
+    names = input(f'Pare {i + 1}: ').split()
+    tree[names[0]] = names[1]
+
+for name in set(tree.keys()).union(tree.values()):
+    height[name] = names_height(tree, name)
+
+print('\nHeight each family member:')
+
+for key in sorted(height.keys()):
+    print(key, height[key])
--- Module19/10_palindrome_again/main.py
+++ Module19/10_palindrome_again/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+string = input('Enter the string: ')
+
+sym_set = set(list(string))
+
+if (len(string) == (2 * len(sym_set) - 1)) or (len(string) == 2 * len(sym_set)):
+    print('Can be made a palindrome')
+else:
+    print("Can't be made a palindrome")
+
