b62d00a1913cb983c368afbf561f7a45ce3f05ae
Kais <kais.bu-hasan@yandex.ru>
2021-06-22 16:37:52 +0300

    added task 1, task 2 not complete

--- Module21/01_challenge_2/main.py
+++ Module21/01_challenge_2/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+def print_num(num):
+    if num == 0:
+        return 0
+
+    print_num(num - 1)
+    print(num)
+    return num + 1
+
+
+number = 100
+print_num(number)
--- Module21/02_my_zip_2/main.py
+++ Module21/02_my_zip_2/main.py
@@ -1,1 +1,27 @@
-# TODO здесь писать код
+def gen(col_1, col_2):
+    generator = (
+        (col_1[index], number)
+        for index, number in enumerate(col_2[:min(len(col_1), len(col_2))])
+    )
+    return generator
+
+
+def output_on_display(i):
+    if i == -1:
+        return -1
+
+    output_on_display(i - 1)
+    print(list(gen_1)[i])
+    return i + 1
+
+
+string = "abcd"
+nums_tuple = 10, 20, 30, 40
+
+print('String: {string}\nTuple of numbers: {tup}'.format(
+    string=string,
+    tup=nums_tuple
+))
+gen_1 = gen(string, nums_tuple)
+print('\nResult:', '\n{el}'.format(el=gen_1))
+output_on_display(len(list(gen_1)) - 1)
