9c2c746e67fbcee976d89c046aaf966b98c02bc3
Kais <kais.bu-hasan@yandex.ru>
2021-06-30 14:53:41 +0300

    added tasks 5..6

--- Module21/05_make_function_faster/main.py
+++ Module21/05_make_function_faster/main.py
@@ -1,9 +1,18 @@
 def calculating_math_func(data):
-    result = 1
-    for index in range(1, data + 1):
-        result *= index
+
+    if data in factorials_dict:
+        result = factorials_dict[data]
+    else:
+        result = max(factorials_dict.values())
+        for index in range(max(factorials_dict.keys()) + 1, data + 1):
+            result *= index
+            factorials_dict[index] = result
+
     result /= data ** 3
     result = result ** 10
     return result
 
-# TODO оптимизировать функцию
+
+factorials_dict = {0: 1}
+number = int(input('Enter the number: '))
+print(calculating_math_func(number))
