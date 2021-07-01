37a0364fe9db3cee2f5dad06a5b0a1eb827bc452
Kais <kais.bu-hasan@yandex.ru>
2021-07-01 10:14:36 +0300

    added task 9

--- Module21/09_hanoi_towers/main.py
+++ Module21/09_hanoi_towers/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+def move(number, start, end):
+    if number > 0:
+        move(number-1, start, 6 - start - end)
+
+        print('Transfer disk {number} from rod number {start} to rod number {end}'.format(
+            number=number,
+            start=start,
+            end=end
+        ))
+
+        move(number-1, 6 - start - end, end)
+
+
+disks_num = int(input('Enter number of disks: '))
+move(disks_num, 1, 3)
