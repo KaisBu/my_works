0e116354b86dca52b940ad978c3b36f8d6838660
Kais <kais.bu-hasan@yandex.ru>
2021-07-03 09:09:00 +0300

    added tasks 1..3, task 4 not complete

--- Module22/02_zen_of_python/main.py
+++ Module22/02_zen_of_python/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+import os
+
+
+def zen_print(path):
+    zen_text = open(path, 'r')
+
+    for line in zen_text:
+        print(line[-1::-1], end='')
+
+    zen_text.close()
+
+
+zen_print(os.path.abspath('zen.txt'))
+
--- Module22/04_files_and_folders/main.py
+++ Module22/04_files_and_folders/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+import os
+
+
+def folders_weight(path, summ_list=[]):
+    if os.path.isfile(path):
+        return os.path.getsize(path)
+
+    for element in os.listdir(path):
+        result = folders_weight(os.path.join(path, element))
+        if result:
+            summ_list.append(result)
+
+    print(sum(summ_list))
+
+
+
+folders_weight(os.path.abspath(os.path.join('..', '..', 'Module22')))
+
