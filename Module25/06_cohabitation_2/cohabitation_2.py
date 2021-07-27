81bea70159d60b8689f2a3e03f6b190f6d932503
Kais <kais.bu-hasan@yandex.ru>
2021-07-27 12:17:03 +0300

    added task 6

--- Module25/06_cohabitation_2/cohabitation_2.py
+++ Module25/06_cohabitation_2/cohabitation_2.py
@@ -1,3 +1,6 @@
+import random
+
+
 class HouseDweller:
 
     def __init__(self, name, house, satiety=50, life=True):
@@ -12,7 +15,9 @@ class HouseDweller:
             print('{} died'.format(self.name))
 
     def __str__(self):
-        return 'Satiety: ' + str(self.satiety) + '\nLife: ' + str(self.life) + str(self.house)
+        return '{}\nName: {}\nSatiety: {}\nLife: {}'.format(
+            str(self.house), self.name, self.satiety, self.life
+        )
 
 
 class Human(HouseDweller):
@@ -46,7 +51,11 @@ class Human(HouseDweller):
             self.satiety += self.house.food
             print('{} is eating'.format(self.name))
         else:
-            print('Not enough food. {} going to the store'.format(self.name))
+            print('Not enough food'.format(self.name))
+
+    def __str__(self):
+        info = super().__str__()
+        return '{}\nHappiness: {}'.format(info, self.happiness)
 
 
 class Husband(Human):
@@ -64,14 +73,187 @@ class Husband(Human):
         self.satiety -= 10
         print('{} is working'.format(self.name))
 
+    def action(self, cat):
+        self.check_satiety()
+        self.check_dirt()
+        self.check_happiness()
+
+        if not self.life:
+            print('{} is ded. Game over!'.format(self.name))
+            return True
+
+        if random.randint(1, 10) == 10:
+            self.pet_the_cat(cat)
+
+        if self.satiety < 30:
+            self.eating()
+        elif self.house.funds < 650:
+            self.working()
+        else:
+            self.playing()
+
+        print(self, '\n')
+        return False
+
+
+class Wife(Human):
+
+    def __init__(self, name, house):
+        super().__init__(name, house)
+
+    def shopping(self):
+        self.satiety -= 10
+        self.house.funds -= 100
+        self.house.food += 100
+        print('{} went to the grocery store'.format(self.name))
+
+    def pet_store(self):
+        self.satiety -= 10
+        self.house.funds -= 100
+        self.house.cat_food += 100
+        print('{} made purchases at a pet store'.format(self.name))
+
+    def buy_fur_coat(self):
+        self.satiety -= 10
+        self.house.funds -= 350
+        self.happiness += 60
+        print('{} bought a fur coat. She is vary happy!'.format(self.name))
+
+    def clean_the_house(self):
+        self.satiety -= 10
+
+        if self.house.dirt < 100:
+            self.house.dirt = 0
+        else:
+            self.house.dirt -= 100
+
+        print('{} cleaned the house'.format(self.name))
+
+    def action(self, cat):
+        self.check_satiety()
+        self.check_dirt()
+        self.check_happiness()
+
+        if not self.life:
+            print('{} is ded. Game over!'.format(self.name))
+            return True
+
+        if random.randint(1, 10) == 10:
+            self.pet_the_cat(cat)
+
+        if self.satiety < 30:
+            self.eating()
+        elif (self.house.food < 100) and (self.house.funds >= 100):
+            self.shopping()
+        elif (self.house.cat_food < 50) and (self.house.funds >= 50):
+            self.pet_store()
+        elif self.house.funds > 450:
+            self.buy_fur_coat()
+        else:
+            self.clean_the_house()
+
+        print(self, '\n')
+        return False
+
+
+class Child(Human):
+
+    def __init__(self, name, house):
+        super().__init__(name, house)
+
+    def walking(self):
+        self.satiety -= 10
+        self.happiness += 5
+        print('{} is walking'.format(self.name))
+
+    def scatter_toys(self):
+        self.house.dirt += 5
+
+    def playing(self):
+        self.satiety -= 10
+        self.happiness += 5
+        self.scatter_toys()
+        print('{} is playing'.format(self.name))
+
+    def action(self, cat):
+        self.check_satiety()
+        self.check_dirt()
+        self.check_happiness()
+
+        if not self.life:
+            print('{} is ded. Game over!'.format(self.name))
+            return True
+
+        if random.randint(1, 10) == 10:
+            self.pet_the_cat(cat)
+
+        if self.satiety < 30:
+            self.eating()
+        elif random.randint(1, 2) == 1:
+            self.playing()
+        else:
+            self.walking()
+
+        print(self, '\n')
+        return False
+
+
+class Cat(HouseDweller):
+
+    def __init__(self, name, house):
+        super().__init__(name, house)
+
+    def eating(self):
+        if self.house.cat_food >= 10:
+            self.house.food -= 10
+            self.satiety += 20
+            print('{} is eating'.format(self.name))
+        elif 0 < self.house.cat_food < 10:
+            self.house.cat_food -= self.house.cat_food
+            self.satiety += self.house.cat_food * 2
+            print('{} is eating'.format(self.name))
+        else:
+            print('Not enough food'.format(self.name))
+
+    def sleep(self):
+        self.satiety -= 10
+        print('{} is sleeping'.format(self.name))
+
+    def tear_wallpaper(self):
+        self.satiety -= 10
+        self.house.dirt += 5
+        print('{} tearing the wallpaper'.format(self.name))
+
+    def action(self):
+        self.check_satiety()
+
+        if not self.life:
+            print('{} is ded. Game over!'.format(self.name))
+            return True
+
+        if self.satiety < 20:
+            self.eating()
+        elif random.randint(1, 2) == 1:
+            self.tear_wallpaper()
+        else:
+            self.sleep()
+
+        print(self, '\n')
+        return False
+
 
 class House:
 
-    def __init__(self, food=50, cat_food=20, funds=0, dirt=0):
-        self.food = food
-        self.cat_food = cat_food
-        self.funds = funds
-        self.dirt = dirt
+    def __init__(self):
+        self.food = 50
+        self.cat_food = 30
+        self.funds = 100
+        self.dirt = 0
 
     def __str__(self):
-        return '\nFood: ' + str(self.food) + '\nFunds: ' + str(self.funds)
+        return '\nFood: {}\nFunds: {}\nCat food: {}\nDirt: {}'.format(
+            self.food, self.funds, self.cat_food, self.dirt
+        )
+
+    def daily_dust(self):
+        self.dirt += 5
--- Module25/06_cohabitation_2/main.py
+++ Module25/06_cohabitation_2/main.py
@@ -1,1 +1,35 @@
-# TODO здесь писать код
+from cohabitation_2 import *
+import random
+
+
+def game():
+    game_over = False
+
+    for i in range(365):
+        print(f'\nDay {i+1}:')
+        family_house.daily_dust()
+
+        for member in roommates:
+            if isinstance(member, Husband) or isinstance(member, Wife) or isinstance(member, Child):
+                game_over = member.action(roommates[random.randint(3, 5)])
+            else:
+                game_over = member.action()
+
+            if game_over:
+                break
+        if game_over:
+            break
+
+
+def roommates_list():
+    for index, i_member in enumerate(all_family):
+        name = input('Enter {} name: '.format(str_list[index]))
+        roommates.append(i_member(name, family_house))
+
+
+roommates = []
+all_family = (Husband, Wife, Child, Cat, Cat, Cat)
+str_list = ["husband's", "wife's", "child's", "first cat's", "second cat's", "third cat's"]
+family_house = House()
+roommates_list()
+game()
