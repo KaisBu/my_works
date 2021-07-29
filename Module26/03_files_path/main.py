048f4e9cb49a395887b60934ff0274c5d5dc3e79
Kais <kais.bu-hasan@yandex.ru>
2021-07-29 14:27:56 +0300

    added tasks 1..2, 3 not complete

--- Module26/03_files_path/main.py
+++ Module26/03_files_path/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+import os
+
+
+def path_gen(folder, file_name):
+
+    for element in os.listdir(folder):
+        path = os.path.join(folder, element)
+
+        if os.path.isfile(path):
+            print(path)
+            if element == file_name:
+                break
+            return path
+        elif os.path.isdir(path):
+            path_gen(path, file_name)
+
+
+path = os.path.abspath(os.path.join(os.path.sep, 'Users', 'Испытатель'))
+needed_file = 'main.py'
+
+path_gen(path, needed_file)
+
