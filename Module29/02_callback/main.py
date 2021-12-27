f97e752d34c383b6d6c23ee31db7edfdb5ac5d12
Kais <kais.bu-hasan@yandex.ru>
2021-12-27 14:10:22 +0300

    added task 1..3, task 3 with flaws

--- Module29/02_callback/main.py
+++ Module29/02_callback/main.py
@@ -1,1 +1,34 @@
-# TODO здесь писать код
+from typing import Callable, Any
+import functools
+
+
+def callback(sym: str) -> Callable:
+    def callback_1(func: Callable) -> Callable:
+
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs) -> Any:
+            if sym == '//':
+                return func(*args, *kwargs)
+            else:
+                return None
+        return wrapped
+    return callback_1
+
+
+@callback('//')
+def example() -> str:
+    print('Пример функции, которая возвращает ответ сервера')
+    return 'OK'
+
+
+app = {
+    '//': example
+}
+
+route = app.get('//')
+if route:
+    response = route()
+    print('Ответ:', response)
+else:
+    print('Такого пути нет')
+
