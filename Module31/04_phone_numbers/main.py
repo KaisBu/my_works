7a6e9648b889e51b53d836cd6aa88b853791d6a9
Kais <kais.bu-hasan@yandex.ru>
2022-01-14 11:37:32 +0300

    added tasks 4..6

--- Module31/04_phone_numbers/main.py
+++ Module31/04_phone_numbers/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+import re
+
+
+telephone_list = ['9999999999', '999999-999', '99999x9999', '7999999999']
+
+for i_num in telephone_list:
+    if len(re.findall(r'\b[8,9]\d{9}\b', i_num)) > 0:
+        print(telephone_list.index(i_num) + 1, 'номер: все в порядке')
+    else:
+        print(telephone_list.index(i_num) + 1, 'номер: не подходит')
